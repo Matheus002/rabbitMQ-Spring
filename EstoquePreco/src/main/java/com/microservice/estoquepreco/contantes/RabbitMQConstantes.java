3c18f880fcc6c929995ebb32e4858f64689194bd
Matheus002 <matheus002_@hotmail.com>
2021-10-01 06:26:20 -0300

    chore: create queue and inicialization

---
+++ EstoquePreco/src/main/java/com/microservice/estoquepreco/contantes/RabbitMQConstantes.java
@@ -0,0 +1,6 @@
+package com.microservice.estoquepreco.contantes;
+
+public class RabbitMQConstantes {
+    public static final String FILA_ESTOQUE = "ESTOQUE";
+    public static final String FILA_PRECO = "PRECO";
+}
